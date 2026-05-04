58bd0cf24b5e1e10d971c82500482664294fc475
BitsortDev <bitsortgraphics123@gmail.com>
2026-05-04 09:57:17 +0100

    fully responsive

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,66 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    const   brand = 'Medical Blog';
+  const brand = "Medical Blog";
+  const [open, setOpen] = useState(false);
 
-    return ( 
-        <div className="navBar">
-            <div className="brand">
-                <h1>{brand}</h1>
-            </div>
-            <div className="links">
-                <ul>
-                    <li><Link to="/"><span className="material-symbols-outlined">home</span>Home</Link></li>
-                    <li><a href="www.home.com"><span className="material-symbols-outlined">news</span>Blog</a></li>
-                    <li><a href="www.home.com"><span className="material-symbols-outlined">article_person</span>About Us</a></li>
-                    <li><a href="www.home.com"><span className="material-symbols-outlined">work_history</span>Jobs</a></li>
-                    <li><a href="www.home.com"><span className="material-symbols-outlined">data_check</span>Verify Licence</a></li>
-                </ul>
-            </div>
-        </div>
-     ); 
-}
- 
+  return (
+    <div className="navBar">
+      
+      {/* BRAND */}
+      <div className="brand">
+        <h1>{brand}</h1>
+      </div>
+
+      {/* HAMBURGER ICON */}
+      <div className="menuIcon" onClick={() => setOpen(!open)}>
+        <span className="material-symbols-outlined">
+          {open ? "close" : "menu"}
+        </span>
+      </div>
+
+      {/* LINKS */}
+      <div className={`links ${open ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link to="/">
+              <span className="material-symbols-outlined">home</span>
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <a href="wwww">
+              <span className="material-symbols-outlined">news</span>
+              Blog
+            </a>
+          </li>
+
+          <li>
+            <a href="wwww">
+              <span className="material-symbols-outlined">article</span>
+              About Us
+            </a>
+          </li>
+
+          <li>
+            <a href="wwww">
+              <span className="material-symbols-outlined">work_history</span>
+              Jobs
+            </a>
+          </li>
+
+          <li>
+            <a href="wwww">
+              <span className="material-symbols-outlined">verified_user</span>
+              Verify Licence
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 export default NavBar;
-
